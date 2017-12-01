da645d836397c19401351f64d9a0753953522456
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2017-12-01 22:08:55 +0100

    Solutions day 1

---
+++ Day 1/solutions.py
@@ -0,0 +1,13 @@
+def sum(sequence, offset):
+	result = 0;
+	for i in range(0,len(sequence)):
+		index2 = (i + offset) % len(sequence);
+		if (sequence[i] == sequence[index2]):
+			result = result + int(sequence[i]);
+
+	return result;
+
+sequence = list(raw_input());
+
+print str(sum(sequence, 1));
+print str(sum(sequence, len(sequence)/2));
