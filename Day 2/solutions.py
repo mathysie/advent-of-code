57533d6579f318dc9ee468b515e5708a4f5032b9
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2017-12-02 13:54:22 +0100

    Solution part 1

---
+++ Day 2/solutions.py
@@ -0,0 +1,21 @@
+def read_input ():
+	spreadsheet = []
+	while True:
+	    try:
+	        line = [int(i) for i in raw_input().split(" ")]
+	    except EOFError:
+	        break
+	    spreadsheet.append(line)
+
+	return spreadsheet
+
+def calc_checksum(sheet):
+	checksum = 0
+
+	for i in range(0,len(sheet)):
+		checksum += max(sheet[i]) - min(sheet[i]);
+
+	return checksum
+
+sheet = read_input()
+print calc_checksum(sheet)
