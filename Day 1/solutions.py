987df8159ebc7ceaaba98530a512ca66a1a327bd
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2017-12-02 15:50:05 +0100

    Implement PEP 8 code style

--- Day 1/solutions.py
+++ Day 1/solutions.py
@@ -1,13 +1,13 @@
 def sum(sequence, offset):
-	result = 0;
-	for i in range(0,len(sequence)):
-		index2 = (i + offset) % len(sequence);
-		if (sequence[i] == sequence[index2]):
-			result = result + int(sequence[i]);
+    result = 0
+    for i in range(0, len(sequence)):
+        index2 = (i + offset) % len(sequence)
+        if (sequence[i] == sequence[index2]):
+            result = result + int(sequence[i])
 
-	return result;
+    return result
 
-sequence = list(raw_input());
+sequence = list(raw_input())
 
-print str(sum(sequence, 1));
-print str(sum(sequence, len(sequence)/2));
+print str(sum(sequence, 1))
+print str(sum(sequence, len(sequence) / 2))
--- Day 2/solutions.py
+++ Day 2/solutions.py
@@ -1,38 +1,40 @@
-def read_input ():
-	spreadsheet = []
-	while True:
-		try:
-			line = [int(i) for i in raw_input().split('\t')]
-		except EOFError:
-			break
-		spreadsheet.append(line)
+def read_input():
+    spreadsheet = []
+    while True:
+        try:
+            line = [int(i) for i in raw_input().split('\t')]
+        except EOFError:
+            break
+        spreadsheet.append(line)
 
-	return spreadsheet
+    return spreadsheet
+
 
 def calc_minmaxsum(sheet):
-	checksum = 0
+    checksum = 0
 
-	for i in range(0,len(sheet)):
-		checksum += max(sheet[i]) - min(sheet[i]);
+    for i in range(0, len(sheet)):
+        checksum += max(sheet[i]) - min(sheet[i])
 
-	return checksum
+    return checksum
+
 
 def calc_evenlydevidesum(sheet):
-	checksum = 0
+    checksum = 0
 
-	for i in range(0, len(sheet)):
-		sheet[i].sort(reverse=True)
-		Found = False
-		for j in range(0, len(sheet[i])):
-			for k in range(j + 1, len(sheet[i])):
-				if (sheet[i][j] % sheet[i][k] == 0):
-					checksum += sheet[i][j] / sheet[i][k]
-					Found = True
-					break
-			if (Found):
-				break
+    for i in range(0, len(sheet)):
+        sheet[i].sort(reverse=True)
+        Found = False
+        for j in range(0, len(sheet[i])):
+            for k in range(j + 1, len(sheet[i])):
+                if (sheet[i][j] % sheet[i][k] == 0):
+                    checksum += sheet[i][j] / sheet[i][k]
+                    Found = True
+                    break
+            if (Found):
+                break
 
-	return checksum
+    return checksum
 
 sheet = read_input()
 print calc_minmaxsum(sheet)
