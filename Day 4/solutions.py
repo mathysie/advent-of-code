7e01237b734ccdc3c91f9c3fe8eb10204ea88c16
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2017-12-04 13:20:33 +0100

    Solutions part 2 of Day 4

--- Day 4/solutions.py
+++ Day 4/solutions.py
@@ -1,13 +1,31 @@
 def count_valid_passphrases():
     counter = 0
+    counter_new = 0
     while True:
         try:
             line = raw_input().split(" ")
             counter += passphrase_isvalid(line)
+            counter_new += passphrase_isvalid_new(line)
         except EOFError:
             break
 
-    return counter
+    print counter
+    print counter_new
+
+
+def passphrase_isvalid_new(passphrase):
+    valid = True
+    for i in range(0, len(passphrase)):
+        passphrase[i] = ''.join(sorted(passphrase[i]))
+        for j in range(i + 1, len(passphrase)):
+            passphrase[j] = ''.join(sorted(passphrase[j]))
+            if (passphrase[i] == passphrase[j]):
+                valid = False
+                break
+        if (not valid):
+            break
+
+    return int(valid)
 
 
 def passphrase_isvalid(passphrase):
@@ -22,4 +40,4 @@ def passphrase_isvalid(passphrase):
 
     return int(valid)
 
-print count_valid_passphrases()
+count_valid_passphrases()
