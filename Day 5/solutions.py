b3a31243ea73fc2c52a3562dfd8d396ba9439826
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2017-12-05 08:45:59 +0100

    Solutions part 2 of Day 5

--- Day 5/solutions.py
+++ Day 5/solutions.py
@@ -20,5 +20,22 @@ def steps_counter(jumps):
 
     return steps
 
+
+def steps_counter_new(jumps):
+    steps = 0
+    i = 0
+    while (i >= 0 and i < len(jumps)):
+        temp = i
+        i += jumps[i]
+        if (jumps[temp] >= 3):
+            jumps[temp] -= 1
+        else:
+            jumps[temp] += 1
+        steps += 1
+
+    return steps
+
 jumps = read_input()
+jumps_new = list(jumps)
 print steps_counter(jumps)
+print steps_counter_new(jumps_new)
