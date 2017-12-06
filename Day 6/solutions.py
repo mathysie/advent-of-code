7aef95d9b04eb5a7e2f97adadc05b3c73152c02c
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2017-12-06 12:13:52 +0100

    Codestyle changes Day 6

--- Day 6/solutions.py
+++ Day 6/solutions.py
@@ -2,29 +2,30 @@ import numpy as np
 
 
 def amount_cycles(sequence):
-    count = 0
     memory = [sequence]
     cycle = False
-    while True:
-        newsequence = list(memory[count])
-        index = np.argmax(newsequence)
-        blocks = newsequence[index]
-        newsequence[index] = 0
-        for i in range(0, blocks):
-            newsequence[(index + i + 1) % len(newsequence)] += 1
-        memory.append(newsequence)
-        count += 1
-        for i in range(0, count):
-            if (memory[i] == memory[count]):
-                print count - i
-                cycle = True
-                break
-        if (cycle):
-            break
 
-    return count
+    while not cycle:
+        newsequence = list(memory[len(memory) - 1])
+        max_index = np.argmax(newsequence)
+        blocks = newsequence[max_index]
+
+        # Redistribute blocks
+        newsequence[max_index] = 0
+        for i in range(0, blocks):
+            newsequence[(max_index + i + 1) % len(newsequence)] += 1
+
+        # Add new sequence to memory
+        memory.append(newsequence)
+
+        # Check if new sequence has been found before
+        for i in range(0, len(memory) - 1):
+            if (memory[i] == memory[len(memory) - 1]):
+                print len(memory) - 1  # Print the amount of cycles before loop
+                print len(memory) - 1 - i  # Print the length of the loop
+                cycle = True
 
 
 sequence = raw_input().split('\t')
 sequence = [int(i) for i in sequence]
-print amount_cycles(sequence)
+amount_cycles(sequence)
