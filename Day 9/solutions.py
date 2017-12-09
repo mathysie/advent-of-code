daa213aad3ccecf16c55020de81b6dad0a58c047
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2017-12-09 12:03:47 +0100

    Solution part 2 of Day 9

--- Day 9/solutions.py
+++ Day 9/solutions.py
@@ -2,23 +2,26 @@ def count_score(stream):
     score = 0
     level = 0
     is_comment = False
+    garbage_count = 0
 
     it = iter(range(0, len(stream)))
     for i in it:
         if (stream[i] == "!"):
             it.next()
-        elif (stream[i] == "<"):
+        elif (stream[i] == "<" and not is_comment):
             is_comment = True
         elif (stream[i] == ">" and is_comment):
             is_comment = False
-        elif (not is_comment):
-            if (stream[i] == "{"):
-                level += 1
-            elif (stream[i] == "}"):
-                score += level
-                level -= 1
+        elif (is_comment):
+            garbage_count += 1
+        elif (stream[i] == "{"):
+            level += 1
+        elif (stream[i] == "}"):
+            score += level
+            level -= 1
 
-    return score
+    print score
+    print garbage_count
 
 stream = raw_input()
-print count_score(stream)
+count_score(stream)
