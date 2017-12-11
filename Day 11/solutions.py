0a2a0d4e1bcc7e25b0186c37d3ef6ccfcb39ea3d
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2017-12-11 12:38:16 +0100

    Solution part 2 of Day 12

--- Day 11/solutions.py
+++ Day 11/solutions.py
@@ -1,10 +1,18 @@
 from math import floor
 
 
-def amount_steps(directions):
+def dist(x, y):
+    if (x * y < 0):
+        return max(abs(x), abs(y))
+    else:
+        return abs(x) + abs(y)
+
+
+def max_dist(directions):
     x = 0
     y = 0
     steps = 0
+    max_distance = 0
 
     for step in directions:
         if (step == 'n'):
@@ -21,12 +29,12 @@ def amount_steps(directions):
             x -= 1
         elif (step == 'sw'):
             x -= 1
+        if (dist(x, y) > max_distance):
+            max_distance = dist(x, y)
 
-    if (x * y < 0):
-        return max(abs(x), abs(y))
-    else:
-        return abs(x) + abs(y)
+    print dist(x, y)
+    return max_distance
 
 
 directions = raw_input().split(",")
-print amount_steps(directions)
+print max_dist(directions)
