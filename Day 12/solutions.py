72af17d7a5f2784189383975460336c5f82d25db
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2017-12-12 21:19:01 +0100

    Solution part 2 of Day 12

--- Day 12/solutions.py
+++ Day 12/solutions.py
@@ -10,20 +10,36 @@ def read_input():
     return pipes
 
 
-def count_programs(pipes):
-    group = set([0])
-    new = set(pipes[0])
+def count_groups(pipes):
+    print len(form_group(pipes, 0))
+
+    group_count = 0
+    numbers = set(range(0, len(pipes)))
+    groups = []
+
+    while len(numbers) > 0:
+        group = form_group(pipes, list(numbers)[0])
+        groups.append(group)
+        numbers -= set(group)
+        group_count += 1
+
+    return group_count
+
+
+def form_group(pipes, start):
+    group = set([start])
+    new = set(pipes[start])
 
     while (len(new) > 0):
         temp = set(new)
         for i in new:
             temp |= pipes[i] - group
-            group |= pipes[i]
+            group |= pipes[i] | set([i])
             temp.remove(i)
         new = set(temp)
 
-    return len(group)
+    return group
 
 
 pipes = read_input()
-print count_programs(pipes)
+print count_groups(pipes)
