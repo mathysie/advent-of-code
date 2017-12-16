370ef9e26a9052ac4fdd412793cdc29384b72ccd
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2017-12-16 14:39:44 +0100

    Solution part 2 of Day 16

--- Day 16/solutions.py
+++ Day 16/solutions.py
@@ -20,10 +20,7 @@ def partner(positions, programs):
     positions[index1], positions[index2] = positions[index2], positions[index1]
 
 
-def order(moves):
-    positions = ["a", "b", "c", "d", "e", "f", "g",
-                 "h", "i", "j", "k", "l", "m", "n", "o", "p"]
-
+def dance_round(positions):
     for move in moves:
         if (move[0] == "s"):
             positions = spin(positions, move[1:])
@@ -32,7 +29,33 @@ def order(moves):
         elif (move[0] == "p"):
             partner(positions, move[1:])
 
-    return "".join(positions)
+    return positions
+
+
+def order():
+    N = 0
+    new_positions = list(positions)
+
+    while True:
+        new_positions = dance_round(new_positions)
+        N += 1
+        if new_positions == positions:
+            break
+
+    return N
+
+
+def dance():
+    N = order()
+
+    new_positions = list(positions)
+    for i in range(0, 10 ** 9 % N):
+        new_positions = dance_round(list(new_positions))
+
+    return "".join(new_positions)
 
 moves = read_input()
-print order(moves)
+positions = ["a", "b", "c", "d", "e", "f", "g",
+             "h", "i", "j", "k", "l", "m", "n", "o", "p"]
+print "".join(dance_round(positions))
+print dance()
