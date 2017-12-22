880f4733ce900c441fffe67bcfd9b0b31d437f50
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2017-12-22 15:21:15 +0100

    Solution part 1 of Day 21

--- Day 21/solutions.py
+++ Day 21/solutions.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def read_input():
     rules = dict()
     while True:
@@ -10,8 +13,49 @@ def read_input():
     return rules
 
 
+def parse_grid(grid):
+    string = ""
+    rows = []
+    for row in grid:
+        rows.append("".join(row))
+
+    return "/".join(rows)
+
+
+def format_grid(string):
+    grid = []
+    rows = string.split("/")
+    for row in rows:
+        grid.append(list(row))
+
+    return grid
+
+
 def initial_grid():
     return ".#./..#/###"
+
+
+def break_grids(grid):
+    len_grid = grid.find("/")
+    if len_grid % 2 == 0:
+        M = 2
+    elif len_grid % 3 == 0:
+        M = 3
+
+    grids = []
+    N = int(grid.find("/") / M)
+    grid = format_grid(grid)
+    for x in range(0, N):
+        for y in range(0, N):
+            newgrid = []
+            for i in range(0, M):
+                row = []
+                for j in range(0, M):
+                    row.append(grid[M * x + i][M * y + j])
+                newgrid.append(row)
+            grids.append(parse_grid(newgrid))
+
+    return grids
 
 
 def flip(grid):
@@ -26,7 +70,13 @@ def flip(grid):
 def rotate(grid):
     rows = grid.split("/")
     newgrid = []
-    newgrid.append([grid[0][2], grid[1][2], grid[2][2]])
+    for i in range(0, len(rows)):
+        newrow = []
+        for j in range(0, len(rows[i])):
+            newrow.append(rows[len(rows) - j - 1][i])
+        newgrid.append(newrow)
+
+    return parse_grid(newgrid)
 
 
 def find_rule(grid):
@@ -35,27 +85,51 @@ def find_rule(grid):
             if (grid == rule):
                 return rule
             else:
-                if (i == 4):
+                if (i == 3):
                     grid = flip(grid)
                 else:
                     grid = rotate(grid)
 
-    print("No rule found!")
+    print("No rule found for {}!".format(grid))
 
 
-def enhance_grids(grids):
-    for grid in grids:
-        grid = rules[find_rule(grid)][1]
+def form_grid(grids):
+    result = []
+    N = int(sqrt(len(grids)))
+    for k in range(0, N):
+        columns = []
+        for j in range(0, len(grids[0])):
+            rows = []
+            for i in range(0, N):
+                rows.append(grids[k * N + i][j])
+            columns.append("".join(rows))
+        result.append("/".join(columns))
+
+    return "/".join(result)
 
 
-def size_after_5():
+def pixels(grid):
+    counter = 0
+    for i in range(0, len(grid)):
+        if grid[i] == "#":
+            counter += 1
+
+    return counter
+
+
+def size_after(M):
     grid = initial_grid()
-    grids = []
 
-    for i in range(0, 5):
-        enhance_grids(grid)
+    for N in range(0, M):
+        grids = break_grids(grid)
 
-    return len(grid[0])
+        for i in range(0, len(grids)):
+            grids[i] = rules[find_rule(grids[i])].split("/")
+
+        grid = form_grid(grids)
+        print(N)
+
+    return pixels(grid)
 
 rules = read_input()
-print(size_after_5())
+print(size_after(5))
