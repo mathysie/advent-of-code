a8b85f83b81d4415505e9b78f84c20061235f4ba
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2018-12-01 13:12:34 +0100

    Day 1, part 2

--- 2018/Day 1/solutions.cpp
+++ 2018/Day 1/solutions.cpp
@@ -31,14 +31,38 @@ int frequency(vector<int> values) {
   return sum;
 }
 
+int frequency_twice(vector<int> values) {
+  int sum = 0;
+  unsigned int i = 0;
+  vector<int> freqs;
+  int T = true;
+
+  while (T) {
+    sum += values[i];
+    for (unsigned int j = 0; j < freqs.size(); ++j) {
+      if (freqs[j] == sum) {
+        T = false;
+      }
+    }
+    cout << endl;
+    freqs.push_back(sum);
+
+    i = i == values.size() - 1 ? 0 : i + 1;
+  }
+
+  return sum;
+}
+
 int main() {
   vector<int> input;
   int solution1;
+  int solution2;
 
   input = read_input();
   solution1 = frequency(input);
   solution2 = frequency_twice(input);
   cout << "Oplossing 1: " << solution1 << "\n";
+  cout << "Oplossing 2: " << solution2 << "\n";
 
   return 0;
 }
