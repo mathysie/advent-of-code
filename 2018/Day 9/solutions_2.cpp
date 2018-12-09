daf993bde2baab47c0f99afb1570fd679c79a30c
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2018-12-09 18:32:36 +0100

    Gebruik list in plaats van eigen linked list

--- 2018/Day 9/solutions_2.cpp
+++ 2018/Day 9/solutions_2.cpp
@@ -1,16 +1,11 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <string>
 #include <vector>
 
 using namespace std;
-
-struct marble {
-  int value;
-  marble *next;
-  marble *prev;
-};
 
 int *read_input() {
   string line;
@@ -24,70 +19,37 @@ int *read_input() {
   return input;
 }
 
-marble *create_starting_marble() {
-  marble *start = new marble;
-  start->value = 0;
-  start->next = start->prev = start;
-
-  return start;
-}
-
-marble *insert_marble(marble *current, int value) {
-  marble *new_marble = new marble;
-  new_marble->value = value;
-  new_marble->next = current;
-  new_marble->prev = current->prev;
-
-  current->prev->next = new_marble;
-  current->prev = new_marble;
-
-  return new_marble;
-}
-
-marble *remove_marble(marble *current) {
-  current->prev->next = current->next;
-  current->next->prev = current->prev;
-  marble *temp = current;
-  current = current->next;
-  delete temp;
-
-  return current;
-}
-
-void delete_marbles(marble *current) {
-  marble *temp = current->next;
-  while (temp != current) {
-    temp = temp->next;
-    delete temp->prev;
-  }
-
-  delete temp;
-}
-
 int winning_score(int players, int max_points) {
   vector<unsigned int> scores(players);
   fill(scores.begin(), scores.end(), 0);
 
-  marble *current = create_starting_marble();
+  list<int> marbles(1, 0);
+  auto it = marbles.begin();
 
   for (int i = 1; i <= max_points * 100; i++) {
     if (i % 23 != 0) {
-      current = current->next->next;
-      current = insert_marble(current, i);
+      it++;
+      if (it == marbles.end()) {
+        it = marbles.begin();
+      }
+      it++;
+
+      it = marbles.insert(it, i);
     } else {
       int player = (i - 1) % players;
       scores[player] += i;
 
       for (int j = 0; j < 7; j++) {
-        current = current->prev;
+        if (it == marbles.begin()) {
+          it = marbles.end();
+        }
+        it--;
       }
-      scores[player] += current->value;
+      scores[player] += *it;
 
-      current = remove_marble(current);
+      it = marbles.erase(it);
     }
   }
-
-  delete_marbles(current);
 
   return *max_element(scores.begin(), scores.end());
 }
