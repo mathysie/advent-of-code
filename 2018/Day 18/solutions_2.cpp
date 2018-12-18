86a12a9e9bebf4851eb9922c836f1f4b5f0ec2fb
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2018-12-18 13:02:52 +0100

    Solutions part 2 of Day 18

--- 2018/Day 18/solutions_2.cpp
+++ 2018/Day 18/solutions_2.cpp
@@ -1,5 +1,130 @@
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
+char get_index(const vector<char> &v, int x, int y) {
+  int N = (int)sqrt(v.size());
+  if (x < 0 || y < 0 || x >= N || y >= N) {
+    return 0;
+  }
+
+  return v[y + N * x];
+}
+
+vector<char> read_input() {
+  vector<char> input;
+  ifstream file("input.txt");
+  string line;
+
+  while (getline(file, line)) {
+    for (auto c : line) {
+      input.push_back(c);
+    }
+  }
+
+  return input;
+}
+
+void run_iteration(vector<char> &area) {
+  vector<char> copy = area;
+  int N = (int)sqrt(copy.size());
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (get_index(copy, i, j) == '.') {
+        int trees = 0;
+        trees += get_index(copy, i - 1, j - 1) == '|';
+        trees += get_index(copy, i - 1, j) == '|';
+        trees += get_index(copy, i - 1, j + 1) == '|';
+        trees += get_index(copy, i, j - 1) == '|';
+        trees += get_index(copy, i, j + 1) == '|';
+        trees += get_index(copy, i + 1, j - 1) == '|';
+        trees += get_index(copy, i + 1, j) == '|';
+        trees += get_index(copy, i + 1, j + 1) == '|';
+
+        if (trees >= 3) {
+          area[j + N * i] = '|';
+        }
+      } else if (get_index(copy, i, j) == '|') {
+        int lumbers = 0;
+        lumbers += get_index(copy, i - 1, j - 1) == '#';
+        lumbers += get_index(copy, i - 1, j) == '#';
+        lumbers += get_index(copy, i - 1, j + 1) == '#';
+        lumbers += get_index(copy, i, j - 1) == '#';
+        lumbers += get_index(copy, i, j + 1) == '#';
+        lumbers += get_index(copy, i + 1, j - 1) == '#';
+        lumbers += get_index(copy, i + 1, j) == '#';
+        lumbers += get_index(copy, i + 1, j + 1) == '#';
+
+        if (lumbers >= 3) {
+          area[j + N * i] = '#';
+        }
+      } else if (get_index(copy, i, j) == '#') {
+        int lumbers = 0;
+        int trees = 0;
+
+        trees += get_index(copy, i - 1, j - 1) == '|';
+        trees += get_index(copy, i - 1, j) == '|';
+        trees += get_index(copy, i - 1, j + 1) == '|';
+        trees += get_index(copy, i, j - 1) == '|';
+        trees += get_index(copy, i, j + 1) == '|';
+        trees += get_index(copy, i + 1, j - 1) == '|';
+        trees += get_index(copy, i + 1, j) == '|';
+        trees += get_index(copy, i + 1, j + 1) == '|';
+
+        lumbers += get_index(copy, i - 1, j - 1) == '#';
+        lumbers += get_index(copy, i - 1, j) == '#';
+        lumbers += get_index(copy, i - 1, j + 1) == '#';
+        lumbers += get_index(copy, i, j - 1) == '#';
+        lumbers += get_index(copy, i, j + 1) == '#';
+        lumbers += get_index(copy, i + 1, j - 1) == '#';
+        lumbers += get_index(copy, i + 1, j) == '#';
+        lumbers += get_index(copy, i + 1, j + 1) == '#';
+
+        if (lumbers == 0 || trees == 0) {
+          area[j + N * i] = '.';
+        }
+      }
+    }
+  }
+}
+
+void run_area(vector<char> &area) {
+  for (int i = 0; i < 1000; i++) {
+    run_iteration(area);
+  }
+  vector<char> copy = area;
+
+  int k = 0;
+  do {
+    run_iteration(copy);
+    k++;
+  } while (area != copy);
+
+  for (int i = 0; i < ((1000000000-1000) % k); i++) {
+    run_iteration(area);
+  };
+}
+
+int resource_value(const vector<char> &area) {
+  int trees = 0;
+  int lumbers = 0;
+
+  for (auto c : area) {
+    trees += c == '|';
+    lumbers += c == '#';
+  }
+
+  return trees * lumbers;
+}
+
 int main() {
+  vector<char> input = read_input();
+  run_area(input);
+  cout << resource_value(input) << endl;
+
   return 0;
 }
