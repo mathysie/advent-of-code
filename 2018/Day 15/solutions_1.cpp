9c6a664c0f835a31e0e34c8cfe76c232631b8e67
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2018-12-23 11:55:07 +0100

    Betere timing code

--- 2018/Day 15/solutions_1.cpp
+++ 2018/Day 15/solutions_1.cpp
@@ -1,5 +1,18 @@
+#include <chrono>
+#include <iostream>
+
 using namespace std;
 
+void display_time(const auto &start) {
+  auto stop = chrono::high_resolution_clock::now();
+
+  auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+  cout << duration.count() << " milliseconds" << endl;
+}
+
 int main() {
+  auto start = chrono::high_resolution_clock::now();
+
+  display_time(start);
   return 0;
 }
